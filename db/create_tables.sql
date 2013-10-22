d7df6978807343092ae14e2ed02e2d7efdf21b30
Feosen <iva588@yandex.ru>
2013-10-23 01:07:26 +0400

    From Kalle Vilmer

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,51 +1,16 @@
-/*
-	Tables
-*/
+CREATE TABLE  tw_user (
+   user_id bigint NOT NULL, 
+   username varchar(20) NOT NULL,
+   screen_name varchar(20) NOT NULL,
+   profile_image_url varchar(255) NOT NULL,
+   PRIMARY KEY(user_id) 
+)
 
-CREATE TABLE users (
-    screen_name character varying,			-- Name from Twitter.
-    id bigint NOT NULL,						-- User id from Twitter.
-    profile_image_url character varying		-- Link o the last user avatar image.
-);
-
-CREATE TABLE posts (
-    user_id bigint,							-- User id from Twitter.
-    post_id bigint NOT NULL,				-- Post id from Twitter.
-    "time" timestamp with time zone,		-- Post creation time from Twitter.
-    coordinates_box box,					-- North-West and South-Ost coordinates from Twtter (lat, long).
-    coordinates point,						-- Calculate from coordinates_box for maps support.
-    text character varying					-- Post text from Twitter.
-);
-
-/*
-	Owners
-*/
-
-ALTER TABLE public.posts OWNER TO postgres;
-ALTER TABLE public.users OWNER TO postgres;
-
-/*
-	Keys
-*/
-
-ALTER TABLE ONLY users
-    ADD CONSTRAINT user_pk PRIMARY KEY (id);
-
-ALTER TABLE ONLY posts
-    ADD CONSTRAINT posts_pk PRIMARY KEY (post_id);
-ALTER TABLE ONLY posts
-    ADD CONSTRAINT posts_fk FOREIGN KEY (user_id) REFERENCES users(id);
-
-/*
-	Comments
-*/
-
-COMMENT ON COLUMN users.screen_name IS 'Name from Twitter.';
-COMMENT ON COLUMN users.id IS 'User id from Twitter.';
-COMMENT ON COLUMN users.profile_image_url IS 'Link o the last user avatar image.';
-COMMENT ON COLUMN posts.user_id IS 'User id from Twitter.';
-COMMENT ON COLUMN posts.post_id IS 'Post id from Twitter.';
-COMMENT ON COLUMN posts.time IS 'Post creation time from Twitter.';
-COMMENT ON COLUMN posts.coordinates_box IS 'North-West and South-Ost coordinates from Twtter (lat, long).';
-COMMENT ON COLUMN posts.coordinates IS 'Calculate from coordinates_box for maps support.';
-COMMENT ON COLUMN posts.text IS 'Post text from Twitter.';
+CREATE TABLE tw_tweet ( 
+tweet_id bigint NOT NULL, 
+tweet_text varchar(140), 
+tweet_date timestamp NOT NULL, 
+coordinates point NOT NULL, 
+place_full_name varchar(255),
+user_id bigint NOT NULL references tw_user(user_id), 
+PRIMARY KEY(tweet_id) )
