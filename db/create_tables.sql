acde38951d543e2365b743e778b3dd3214c7c053
Feosen <iva588@yandex.ru>
2013-10-24 11:25:35 +0400

    Syntax errors fix.
    Also added database creation.

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,16 +1,23 @@
-CREATE TABLE  tw_user (
-   user_id bigint NOT NULL, 
+CREATE DATABASE geotwitter;
+
+USE geotwitter;
+
+CREATE TABLE  tw_user 
+(
+   user_id bigint NOT NULL,
    username varchar(20) NOT NULL,
    screen_name varchar(20) NOT NULL,
    profile_image_url varchar(255) NOT NULL,
    PRIMARY KEY(user_id) 
-)
+);
 
-CREATE TABLE tw_tweet ( 
+CREATE TABLE tw_tweet 
+( 
 tweet_id bigint NOT NULL, 
 tweet_text varchar(140), 
 tweet_date timestamp NOT NULL, 
 coordinates point NOT NULL, 
 place_full_name varchar(255),
 user_id bigint NOT NULL references tw_user(user_id), 
-PRIMARY KEY(tweet_id) )
+PRIMARY KEY(tweet_id) 
+);
