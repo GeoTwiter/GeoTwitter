2f5f2767da85671162dbbe312ffd260423bd25c9
Petr <petrtemnikov@gmail.com>
2013-11-19 20:07:13 +0400

    Revert "Update"
    This reverts commit 41252b25245fc5cc56c687d78ae8afa1801e0ffd.

--- parser/sample.py
+++ parser/sample.py
@@ -6,8 +6,8 @@ test.connect_to_twitter_stream()
 test.stream.parse()
 
 # Using REST for user's tweets collection
-test.connect_to_twitter_rest()
 test.rest.parse_twitter_user(1958892991)
+test.rest.parse()
 
 # Using REST to collect tweets of users in the database
 test.connect_to_twitter_rest()
